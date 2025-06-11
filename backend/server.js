106a17ee36937c44d2113b8818f44e32a7cd6f42
Gosia Magdzik <melassa26@gmail.com>
2025-06-11 19:47:56 +0200

    connect to MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 //const express = require('express');  "type": "module" insted of commonjs
-
 import express from 'express';
+import dotenv from 'dotenv';
+import { connectDB } from './config/db.js';
 
+dotenv.config();
 const app = express();
 
 //listening for get method "/" - route 
@@ -9,6 +11,10 @@ app.get("/", (req, res) => {
     res.send("server is ready Hello");
 });
 
+console.log(process.env.MONGO_URL);
+
 app.listen(3000, () => {
+    connectDB();
     console.log("server Started at 5000");
 })
+
