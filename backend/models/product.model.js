54ece0fcd7256311363be4898f486efaa9585163
Gosia Magdzik <melassa26@gmail.com>
2025-06-12 18:09:51 +0200

    typo

--- backend/models/product.model.js
+++ backend/models/product.model.js
@@ -19,5 +19,5 @@ const productSchema = new mongoose.Schema({
                         //updatedAt – kiedy dokument był ostatnio zmieniony
 });
 
-const Product = mongoose.model('Prosuct', productSchema);
+const Product = mongoose.model('Product', productSchema);
 export default Product;
