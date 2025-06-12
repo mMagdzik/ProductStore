b9ab81e37412c3ec1a81746bb46bc5cecbe8d12b
Gosia Magdzik <melassa26@gmail.com>
2025-06-12 18:39:24 +0200

    post request has been created and tested with Postman

--- backend/server.js
+++ backend/server.js
@@ -2,13 +2,28 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
+import Product from "./models/product.model.js"
+
 
 dotenv.config();
 const app = express();
 
-//listening for get method "/" - route 
-app.get("/", (req, res) => {
-    res.send("server is ready Hello");
+app.use(express.json()); // middleware allows to accept JSON in req.body
+
+app.post("/api/products", async(req, res) => {
+    const product = req.body; //user'll send this data
+
+    if(!product.name || !product.price || !product.image) {
+        return res.status(400).json({success: false, message: "Please provide all fields" })
+    }
+    const newProduct = new Product(product);
+    
+    try {
+        await newProduct.save();
+        res.status(201).json({ success: true, data: newProduct });
+    } catch (error) {
+        console.error(500).json({ success: false, message: "Server Error" })
+    }
 });
 
 console.log(process.env.MONGO_URL);
