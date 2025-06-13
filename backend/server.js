b3e90bd6f6835413f7a2c578925a1f130fe76a04
Gosia Magdzik <melassa26@gmail.com>
2025-06-13 12:24:25 +0200

    handling req. error

--- backend/server.js
+++ backend/server.js
@@ -33,8 +33,9 @@ app.delete("/api/products/:id", async (req, res) => {
     const { id } = req.params;
     try {
         await Product.findByIdAndDelete(id);
-    } catch ( error ) {
-
+        res.status(200).json({ success: true, message: "Product deleted" })
+    }   catch ( error ) {
+        res.status(404).json({ success: false, message: "Product not found" })
     }
 })
 
