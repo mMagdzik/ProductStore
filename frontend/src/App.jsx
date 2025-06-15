5062ed997d6ca117e3f99ab2a2d2548e57f150b1
Gosia Magdzik <melassa26@gmail.com>
2025-06-15 17:39:22 +0200

    App structure has been created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,17 @@
-import { Button } from "@chakra-ui/react"
+import { Button, Box } from "@chakra-ui/react";
+import { Routes, Route, createPath } from "react-router-dom";
+
 
 function App() {
 
   return (
-    <>
-      <Button>
-          Hello World
-      </Button>
-    </>
+    <Box minH={"100vh"}>
+      {/* <Navbar> */}
+      <Routes>
+        <Route path = "/" element={ HomePage }/>
+        <Route path = "/" element={ createPage }/>
+      </Routes>
+    </Box>
   )
 }
 
