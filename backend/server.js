f2e605ec16f142457117e28dd57baff547f8cf05
Gosia Magdzik <melassa26@gmail.com>
2025-06-15 12:55:39 +0200

    Getting port from .env instead hardcodding

--- backend/server.js
+++ backend/server.js
@@ -6,13 +6,14 @@ import productRoutes from "./routes/product.route.js"
 
 dotenv.config();
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json()); // middleware allows to accept JSON in req.body
 
 app.use("/api/products", productRoutes);
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
     connectDB();
-    console.log("server Started at 3000");
+    console.log("server Started at " + PORT);
 })
 
