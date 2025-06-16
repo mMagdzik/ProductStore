5838e8198b36cf265f425accb75e9af3eeb5f4f3
Gosia Magdzik <melassa26@gmail.com>
2025-06-16 15:48:49 +0200

    react Icons installed

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,8 +1,37 @@
-import React from 'react'
+import { Container, HStack, Flex, Text, Button } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <Container maxW="1140px" px={4}>
+      <Flex
+        h={16}
+        alignItems={"center"}
+        justifyContent={"space-between"}
+        flexDir={{
+          base:"column",
+          sm:"row"
+        }}
+      >
+        <Text
+          bgGradient={'linear(to-l, #7928CA, #FF0080)'}
+          bgClip={'text'}
+          fontSize={{ base: "22", sm: "28" }}
+          fontWeight={"bold"}
+          textTransform={"uppercase"}
+          textAlign={"center"}
+        >
+          <Link to={"/"}>Product Store 🛒</Link>
+        </Text>
+        <HStack spacing={2} alignItems={"center"}>
+          <Link to={"/create"}>
+            <Button>
+              
+            </Button>
+          </Link>
+        </HStack>
+      </Flex>
+    </Container>
   )
 }
 
