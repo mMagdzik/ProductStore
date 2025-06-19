3d81d7229289ccf76313b7f86b3e64d31ef0d40f
Gosia Magdzik <melassa26@gmail.com>
2025-06-19 19:14:44 +0200

    readME test

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,21 @@
 //const express = require('express');  "type": "module" insted of commonjs
-import express, { json } from 'express';
-import dotenv from 'dotenv';
-import { connectDB } from './config/db.js';
-import productRoutes from "./routes/product.route.js"
+import express, { json } from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+import productRoutes from "./routes/product.route.js";
+import path from "path";
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.json()); // middleware allows to accept JSON in req.body
+//frontend folder as a static assets
+const __dirname = path.resolve();
+if (process.env.NODE_ENV === "production") app.use(express.json()); // middleware allows to accept JSON in req.body
 
 app.use("/api/products", productRoutes);
 
 app.listen(PORT, () => {
-    connectDB();
-    console.log("server Started at " + PORT);
-})
-
+  connectDB();
+  console.log("server Started at " + PORT);
+});
