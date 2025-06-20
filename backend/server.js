ba638b011caca3fab161c3042fb9aa110861b4a9
Gosia Magdzik <melassa26@gmail.com>
2025-06-20 17:38:18 +0200

    defloyment fix, express 4 version

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,29 @@
 //const express = require('express');  "type": "module" insted of commonjs
 import express, { json } from "express";
 import dotenv from "dotenv";
-import { connectDB } from "./config/db.js";
-import productRoutes from "./routes/product.route.js";
 import path from "path";
 
+import { connectDB } from "./config/db.js";
+
+import productRoutes from "./routes/product.route.js";
+
 dotenv.config();
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-//frontend folder as a static assets
 const __dirname = path.resolve();
+
+app.use(express.json()); // allows us to accept JSON data in the req.body
+
+app.use("/api/products", productRoutes);
+
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/frontend/dist")));
-
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
   });
 } // middleware allows to accept JSON in req.body
-
-app.use("/api/products", productRoutes);
 
 app.listen(PORT, () => {
   connectDB();
